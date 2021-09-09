ceca46cda63cbff931fd8dffd055a2fc4bc38c9f
tienhgm <47660593+tienhgm@users.noreply.github.com>
2021-09-09 12:40:07 +0700

    config

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,9 @@
 import "./index.scss";
 import { Button } from 'antd';
 export default function Header() {
-  return <div className="tien">  <Button type="primary">Primary Button</Button></div>;
+  return <div className="tien">
+    <div className="text-8xl">
+      123456
+    </div>
+  </div>;
 }
