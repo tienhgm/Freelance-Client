a802beddff85fa1d7530f703af2b658af5fa4a45
tienhgm <47660593+tienhgm@users.noreply.github.com>
2021-09-10 21:25:19 +0700

    route

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,22 @@ import NotFound from "components/NotFound";
 import DashboardUser from "features/DashboardUser";
 import Posts from "features/Posts";
 import LandingPage from "features/LandingPage";
+import { useMemo } from "react";
+import Login from "features/Auth";
 function App() {
+  const pathname = window.location.pathname;
+  const hiddenHeader = useMemo(() => {
+    const routerExcept = ["/login", "/register"];
+    const currentRouter = pathname;
+    return routerExcept.indexOf(currentRouter) !== -1;
+  }, [pathname]);
+  console.log(hiddenHeader);
   return (
     <div className="App">
-      <Header />
+      {hiddenHeader ? "" : <Header />}
       <Switch>
         <Route path="/" component={LandingPage} exact />
+        <Route path="/login" component={Login} exact />
         <Route path="/posts" component={Posts} />
         <Route path="/dashboard/user" component={DashboardUser} />
         <Route component={NotFound} />
