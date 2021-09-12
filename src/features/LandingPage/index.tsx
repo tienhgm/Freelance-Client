dbc39660d275308ee982b2c156869e1e4444bb2d
tienhgm <47660593+tienhgm@users.noreply.github.com>
2021-09-12 20:46:54 +0700

    banner

--- src/features/LandingPage/index.tsx
+++ src/features/LandingPage/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Banner from "./components/Banner";
 
 function LandingPage() {
-  return <div className="grid grid-cols-1 mt-5">ladning </div>;
+  return (
+    <div className="container">
+      <Banner />
+    </div>
+  );
 }
 
 export default LandingPage;
