a7a26fd5a5586ae3d4336d2e9b5d07e81c1fbecb
tienhgm <47660593+tienhgm@users.noreply.github.com>
2021-10-10 17:40:24 +0700

    customize theme

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,21 @@
 // craco.config.js
+const CracoLessPlugin = require('craco-less');
 module.exports = {
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
 };
