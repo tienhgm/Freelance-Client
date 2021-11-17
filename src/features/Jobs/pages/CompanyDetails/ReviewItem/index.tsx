0213c388f480284d9a3eb15a7aee2c69454ab062
thientk-1188 <tran.khac.thien@sun-asterisk.com>
2021-11-18 04:40:51 +0700

    edit ui company details, add map api

--- src/features/Jobs/pages/CompanyDetails/ReviewItem/index.tsx
+++ src/features/Jobs/pages/CompanyDetails/ReviewItem/index.tsx
@@ -6,11 +6,13 @@ export default function ReviewItem({
   title,
   content,
   reviewTime,
+  index,
 }: {
   ratingPoint: number;
   title: string;
   content: string;
   reviewTime: string;
+  index: number;
 }) {
   const ratingStar = useMemo(() => {
     let stars: string[] = [];
@@ -26,28 +28,30 @@ export default function ReviewItem({
     return stars;
   }, [ratingPoint]);
   return (
-    <div className="review">
-      <div className="review__title">
-        <h4>{title}</h4>
-      </div>
-      <div className="review__rating flex justify-between">
-        <div className="stars text-sm flex items-center">
-          <div className="rating-point font-bold flex text-white px-2 mr-2 bg-yellow-300 rounded-md">
-            <span className="m-auto">{ratingPoint}</span>
+    <div className={`flex items-center px-6 py-3 ${index % 2 === 0 ? 'bg-gray-100' : ' bg-gray-200'}`}>
+      <div className="review">
+        <div className="review__title">
+          <h4>{title}</h4>
+        </div>
+        <div className="review__rating flex justify-between">
+          <div className="stars text-sm flex items-center">
+            <div className="rating-point font-bold flex text-white px-2 mr-2 bg-yellow-300 rounded-md">
+              <span className="m-auto">{ratingPoint}</span>
+            </div>
+            <div className="ratting-star text-yellow-300">
+              {ratingStar.map((className, key) => {
+                return <i key={key} className={className} />;
+              })}
+            </div>
           </div>
-          <div className="ratting-star text-yellow-300">
-            {ratingStar.map((className, key) => {
-              return <i key={key} className={className} />;
-            })}
+          <div className="review-time flex items-center">
+            <i className="bx bx-calendar mr-2"></i>
+            {reviewTime}
           </div>
         </div>
-        <div className="review-time flex items-center">
-          <i className="bx bx-calendar mr-2"></i>
-          {reviewTime}
+        <div className="review__content my-3">
+          <p>{content}</p>
         </div>
-      </div>
-      <div className="review__content my-3 border-b-2 border-gray-400">
-        <p>{content}</p>
       </div>
     </div>
   );
--- src/features/Jobs/pages/CompanyDetails/index.tsx
+++ src/features/Jobs/pages/CompanyDetails/index.tsx
@@ -1,51 +1,15 @@
+import React from 'react';
 import { useAppDispatch } from 'app/hooks';
 import { handleGetDetailCompany } from 'app/slices/companySlice';
 import JobItem from 'components/JobItem';
 import { useEffect, useState } from 'react';
 import { useRouteMatch } from 'react-router';
-// import React from 'react';
 import JobItemProps from 'types/jobItemProps';
 import { Rate } from 'antd';
 import './index.scss';
 import ReviewItem from './ReviewItem';
 import { FileProtectOutlined, PhoneOutlined } from '@ant-design/icons';
 const jobList: Array<JobItemProps> = [
-  {
-    company: 'Hexagon',
-    companyLogo: 'https://www.vasterad.com/themes/hireo/images/company-logo-01.png',
-    jobTitle: 'Bilingual Event Support Specialist',
-    location: 'San Francisco',
-    jobType: 'Full Time',
-    salary: '$35.000 - $38.000',
-    postTime: '2 days ago',
-  },
-  {
-    company: 'Hexagon',
-    companyLogo: 'https://www.vasterad.com/themes/hireo/images/company-logo-01.png',
-    jobTitle: 'Bilingual Event Support Specialist',
-    location: 'San Francisco',
-    jobType: 'Full Time',
-    salary: '$35.000 - $38.000',
-    postTime: '2 days ago',
-  },
-  {
-    company: 'Hexagon',
-    companyLogo: 'https://www.vasterad.com/themes/hireo/images/company-logo-01.png',
-    jobTitle: 'Bilingual Event Support Specialist',
-    location: 'San Francisco',
-    jobType: 'Full Time',
-    salary: '$35.000 - $38.000',
-    postTime: '2 days ago',
-  },
-  {
-    company: 'Hexagon',
-    companyLogo: 'https://www.vasterad.com/themes/hireo/images/company-logo-01.png',
-    jobTitle: 'Bilingual Event Support Specialist',
-    location: 'San Francisco',
-    jobType: 'Full Time',
-    salary: '$35.000 - $38.000',
-    postTime: '2 days ago',
-  },
   {
     company: 'Hexagon',
     companyLogo: 'https://www.vasterad.com/themes/hireo/images/company-logo-01.png',
@@ -80,19 +44,21 @@ export default function CompanyDetails() {
   const [country, setCountry] = useState<any>('');
   const [rate, setRate] = useState<any>();
   const [info, setInfo] = useState<any>();
+  const [logo, setLogo] = useState();
   const route = useRouteMatch();
   const dispatch = useAppDispatch();
   // @ts-ignore
   let companyId = route.params.id;
   const getDetail = async () => {
     const { payload } = await dispatch(handleGetDetailCompany(companyId));
-    console.log(payload);
     if (!!payload) {
       setCompanyName(payload.name);
       setIsVerified(payload.isVerified);
       setCountry(payload.country);
       setRate(payload.stars);
       setInfo(payload.information);
+      setLogo(payload.logo);
+      console.log(info);
     }
   };
 
@@ -106,13 +72,13 @@ export default function CompanyDetails() {
         <div className="container relative flex flex-col justify-between m-auto page__header items-left lg:items-center lg:flex-row">
           <div className="flex flex-col gap-5 header__left items-left lg:items-center pt-14 lg:py-14 lg:flex-row">
             <div className="shadow-xl company-logo w-min">
-              <img src="https://www.vasterad.com/themes/hireo/images/company-logo-03a.png" alt="king" />
+              <img src={`http://${logo}`} alt={companyName} />
             </div>
             <div className="general-info">
               <h2 className="text-2xl info__job-title">{companyName}</h2>
               <div className="flex gap-4 font-medium">
                 {info && (
-                  <div className="flex items-center">
+                  <div className="flex items-center mr-5">
                     <PhoneOutlined className="mr-2" /> Phone number: {info.phoneNumber}
                   </div>
                 )}
@@ -150,7 +116,7 @@ export default function CompanyDetails() {
       </div>
       <div className="container flex flex-col m-auto content mt-14 lg:flex-row">
         <div className="w-full px-4 lg:pr-10 content__main lg:w-2/3">
-          <div className="job-description" style={{minHeight: '400px'}}>
+          <div className="job-description" style={{ minHeight: '400px' }}>
             <h2 className="mt-2 mb-10 text-xl">About Company</h2>
             {info && (
               <div className="mb-10 text-base text-justify job-description__content">
@@ -159,61 +125,54 @@ export default function CompanyDetails() {
             )}
           </div>
           <div className="mb-10 similar-jobs">
-            <h2 className="flex flex-wrap mt-2 mb-10 text-xl">Other company</h2>
+            <h2 className="mt-2 mb-10 text-xl flex flex-wrap">Open Positions</h2>
             {jobList.map((job, index) => (
-              <div className="inline-block w-full p-3 md:w-1/2">
+              <div className="inline-block w-full p-3">
                 <JobItem {...job} key={index} />
               </div>
             ))}
           </div>
+          <div className="mb-10 location">
+            <h2 className="px-6 py-3 mb-0 text-xl font-normal bg-gray-300">
+              <i className="bx bx-like mr-3"></i>Reviews
+            </h2>
+            <div className="reviews-list overflow-y-auto">
+              <ReviewItem
+                ratingPoint={4.5}
+                title={'Doing things the right way'}
+                content={
+                  'Great company and especially ideal for the career-minded individual. The company is large enough to offer a variety of jobs in all kinds of interesting locations. Even if you never change roles, your job changes and evolves as the company grows, keeping things fresh.'
+                }
+                reviewTime={'August 2019'}
+                index={0}
+              />
+              <ReviewItem
+                ratingPoint={4.5}
+                title={'Doing things the right way'}
+                content={
+                  'Great company and especially ideal for the career-minded individual. The company is large enough to offer a variety of jobs in all kinds of interesting locations. Even if you never change roles, your job changes and evolves as the company grows, keeping things fresh.'
+                }
+                reviewTime={'August 2019'}
+                index={1}
+              />
+            </div>
+          </div>
         </div>
         <div className="flex flex-col w-full gap-10 px-8 content__sidebar lg:w-1/3">
           <div className="w-full mb-12 text-base job-review">
-            <h2 className="px-6 py-3 mb-0 text-xl font-normal bg-gray-300">
-              <i className="mr-3 bx bx-like"></i>Reviews
-            </h2>
-            <div className="overflow-y-auto reviews-list">
-              <div className="flex items-center px-6 py-3 bg-gray-200">
-                <ReviewItem
-                  ratingPoint={4.5}
-                  title={'Doing things the right way'}
-                  content={
-                    'Great company and especially ideal for the career-minded individual. The company is large enough to offer a variety of jobs in all kinds of interesting locations. Even if you never change roles, your job changes and evolves as the company grows, keeping things fresh.'
-                  }
-                  reviewTime={'August 2019'}
-                />
-              </div>
-              <div className="flex items-center px-6 py-3 bg-gray-200">
-                <ReviewItem
-                  ratingPoint={4.5}
-                  title={'Doing things the right way'}
-                  content={
-                    'Great company and especially ideal for the career-minded individual. The company is large enough to offer a variety of jobs in all kinds of interesting locations. Even if you never change roles, your job changes and evolves as the company grows, keeping things fresh.'
-                  }
-                  reviewTime={'August 2019'}
-                />
-              </div>
-              <div className="flex items-center px-6 py-3 bg-gray-200">
-                <ReviewItem
-                  ratingPoint={4.5}
-                  title={'Doing things the right way'}
-                  content={
-                    'Great company and especially ideal for the career-minded individual. The company is large enough to offer a variety of jobs in all kinds of interesting locations. Even if you never change roles, your job changes and evolves as the company grows, keeping things fresh.'
-                  }
-                  reviewTime={'August 2019'}
-                />
-              </div>
-              <div className="flex items-center px-6 py-3 bg-gray-200">
-                <ReviewItem
-                  ratingPoint={4.5}
-                  title={'Doing things the right way'}
-                  content={
-                    'Great company and especially ideal for the career-minded individual. The company is large enough to offer a variety of jobs in all kinds of interesting locations. Even if you never change roles, your job changes and evolves as the company grows, keeping things fresh.'
-                  }
-                  reviewTime={'August 2019'}
-                />
-              </div>
-            </div>
+            <h2 className="mt-2 mb-10 text-xl">Location</h2>
+            {info?.addresses[0] && (
+              <iframe
+                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDAHhcUacKG8mW34H9OPSh54v6ICnTZZMM&q=${info?.addresses[0]}`}
+                height="300"
+                frameBorder="0"
+                title="border:0;"
+                allowFullScreen={true}
+                aria-hidden="false"
+                tabIndex={0}
+                className="w-full"
+              />
+            )}
           </div>
         </div>
       </div>
